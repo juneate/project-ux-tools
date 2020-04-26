4d6a723ab85140761110df1746040ffd08171612
Fabio <fabiog@live.com.pt>
2020-04-26 19:57:08 -0400

    create and append a 'p' element to $element variab

--- js/index.js
+++ js/index.js
@@ -12,9 +12,16 @@ const infininteContent = ()=> {
         const $createHeading = document.createElement('H2');
 
         $createHeading.textContent = 'More content coming soon!';
-        
+
         $element.appendChild($createHeading);
     }
 
+		
+		const $createParagraph = document.createElement('p');
 
+		
+		$createParagraph.textContent = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, quos impedit fugit velit magni quidem, qui molestiae, aliquam sequi nobis repellat null doloremque';
+
+		
+		$element.appendChild($createParagraph);
 }
