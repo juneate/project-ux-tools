d675cc722575bf0a8f6b04ef424a9e02429ab354
Fabio <fabiog@live.com.pt>
2020-04-26 20:12:32 -0400

    change $element from a global to a local variable

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 //create an infiniteContent function 
 
-// find the section with the ID of element
-const $element = document.querySelector('#element')
+
 
 
 const infiniteContent = ()=> {
+
+    // find the section with the ID of element
+    const $element = document.querySelector('#element')
 
     if ($element.getBoundingClientRect().top < window.innerHeight) {
 
